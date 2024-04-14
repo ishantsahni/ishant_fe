f9780b4432e3ffb9d3bac91707bc6057f85fe3b4
Ishant Sahni <ishantsahni888@gmail.com>
2024-04-14 20:13:01 +0530

    axios package added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,16 @@
+import axios from "axios";
+import { useEffect } from "react";
+
 function App() {
+
+  useEffect(() => {
+    axios.get("http://localhost:4000/firstApi").then(response => {
+      console.log("first api response ", response);
+    }).catch(error => {
+      console.error('Error fetching data: ', error);
+    })
+  });
+
   return (
     <div>
       <p >Home Page</p>
