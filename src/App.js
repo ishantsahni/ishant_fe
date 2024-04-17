0b796b95df8bd75c16de63c1b6a597785af0c9a7
Ishant Sahni <ishantsahni888@gmail.com>
2024-04-18 02:15:29 +0530

    formik added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 import { useEffect } from "react";
 import API_URLS from "./config/API_URLS";
+import { useFormik } from "formik";
+import * as yup from 'yup';
 
 function App() {
 
@@ -10,11 +12,68 @@ function App() {
     }).catch(error => {
       console.error('Error fetching data: ', error);
     })
+  }, []);
+
+  const formik = useFormik({
+    initialValues: {
+      name: '',
+      dish: '',
+      color: '',
+      city: ''
+    },
+    validationSchema: yup.object({
+      name: yup.string().required(),
+      dish: yup.string().required(),
+      color: yup.string().required(),
+      city: yup.string().required(),
+    }),
+    onSubmit: values => {
+      console.log("formik values ", values);
+    },
   });
 
   return (
     <div>
       <p >Home Page Changed</p>
+      <form onSubmit={formik.handleSubmit}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <input
+            id="name"
+            name="name"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.name}
+          />
+          <label htmlFor="dish">Dish</label>
+          <input
+            id="dish"
+            name="dish"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.dish}
+          />
+          <label htmlFor="color">Color</label>
+          <input
+            id="color"
+            name="color"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.color}
+          />
+          <label htmlFor="city">City</label>
+          <input
+            id="city"
+            name="city"
+            type="text"
+            onChange={formik.handleChange}
+            value={formik.values.city}
+          />
+          <button
+            type="submit"
+          >Submit</button>
+        </div>
+      </form>
     </div>
   );
 }
