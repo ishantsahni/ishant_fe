911385777ebd7b34178af8b0628d3b1b4cdfd529
Ishant Sahni <ishantsahni888@gmail.com>
2024-04-24 02:58:15 +0530

    asterik sign added

--- src/components/CustomTextField/index.jsx
+++ src/components/CustomTextField/index.jsx
@@ -1,16 +1,17 @@
 import { TextField } from "@mui/material";
 
 function CustomTextField() {
-    return (
-        <div>
-        <TextField
-            id="outlined-basic" 
-            fullWidth
-            label="Outlined"
-            variant="outlined" 
-        />
-          </div>
-    )
+  return (
+    <div>
+      <p className="text-[#607083] font-normal text-xs mb-1">Label<span className="text-[#ffa9a9]">*</span></p>
+      <TextField
+        id="outlined-basic"
+        fullWidth
+        variant="outlined"
+        placeholder="Enter"
+      />
+    </div>
+  );
 }
 
 export default CustomTextField;
