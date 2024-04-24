71479cd83a0353a85cf431ba9595ee345814cab5
Ishant Sahni <ishantsahni888@gmail.com>
2024-04-25 01:21:08 +0530

    default props added

--- src/components/CustomDropdown/index.jsx
+++ src/components/CustomDropdown/index.jsx
@@ -1,8 +1,9 @@
-import React, { useState } from 'react';
-import { Select, MenuItem, FormControl, InputLabel } from '@mui/material';
+import React, { useState } from "react";
+import PropTypes from "prop-types";
+import { Select, MenuItem, FormControl, InputLabel } from "@mui/material";
 
-function CustomDropdown() {
-  const [selectedOption, setSelectedOption] = useState('');
+function CustomDropdown({dropdownOptions}) {
+  const [selectedOption, setSelectedOption] = useState("");
 
   const handleChange = (event) => {
     console.log("event value ", event.target.value);
@@ -12,7 +13,9 @@ function CustomDropdown() {
   return (
     <div>
       <FormControl fullWidth>
-      <p className="text-[#607083] font-normal text-xs mb-1">Select an option<span className="text-[#ffa9a9]">*</span></p>
+        <p className="text-[#607083] font-normal text-xs mb-1">
+          Select an option<span className="text-[#ffa9a9]">*</span>
+        </p>
         <Select
           labelId="dropdown-label"
           id="dropdown"
@@ -20,19 +23,27 @@ function CustomDropdown() {
           onChange={handleChange}
           displayEmpty // Show the empty option as a placeholder
           renderValue={(selected) => {
-            if(!selected) {
-                return <em>Select</em>
+            if (!selected) {
+              return <em>Select</em>;
             }
             return selected;
           }}
         >
-          <MenuItem value="option1">Option 1</MenuItem>
-          <MenuItem value="option2">Option 2</MenuItem>
-          <MenuItem value="option3">Option 3</MenuItem>
+            {dropdownOptions.map((item) => (
+                <MenuItem key={item.value} value={item.value}>{item.label}</MenuItem>
+            ))}
+          
         </Select>
       </FormControl>
     </div>
   );
 }
+
+CustomDropdown.propTypes = {
+//   name: PropTypes.string.isRequired, // Required prop
+};
+
+CustomDropdown.defaultProps = {
+};
 
 export default CustomDropdown;
