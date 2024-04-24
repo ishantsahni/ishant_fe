9b8b294052808bead9b3ed9e0540b2c9c4b0a723
Ishant Sahni <ishantsahni888@gmail.com>
2024-04-24 21:25:49 +0530

    formatting added

--- src/pages/AddUserShoppingDetailsPage/index.js
+++ src/pages/AddUserShoppingDetailsPage/index.js
@@ -1,6 +1,7 @@
 import { useFormik } from "formik";
 import CustomTextField from "../../components/CustomTextField";
 import { Button } from "@mui/material";
+import * as Yup from 'yup';
 
 function AddUserShoppingDetailsPage() {
 
@@ -11,8 +12,19 @@ function AddUserShoppingDetailsPage() {
             age: 0,
             email: '',
         },
+        validationSchema: Yup.object({
+            firstName: Yup.string()
+                .max(15, 'Must be 15 characters or less')
+                .required('Required'),
+            lastName: Yup.string()
+                .max(20, 'Must be 20 characters or less')
+                .required('Required'),
+            age: Yup.number("Must be a number").required("Required"),
+            email: Yup.string().email('Invalid email address').required('Required'),
+        }),
+
         onSubmit: values => {
-            console.log("formik submitted ", formik);
+            console.log("formik submitted ", values);
         },
     });
 
@@ -21,10 +33,35 @@ function AddUserShoppingDetailsPage() {
     return (
         <div className="mt-[50px] mx-[50px]">
             <form>
-                <CustomTextField id="firstName" name="firstName" onChange={formik.handleChange} value={formik.values.firstName} label="First Name" />
-                <CustomTextField id="lastName" name="lastName" onChange={formik.handleChange} value={formik.values.lastName} label="Last Name" />
-                <CustomTextField id="age" name="age" onChange={formik.handleChange} type="number" value={formik.values.age} label="Age" />
-                <CustomTextField id="email" name="email" onChange={formik.handleChange} value={formik.values.email} label="Email" />
+                <CustomTextField
+                    id="firstName"
+                    name="firstName"
+                    onChange={formik.handleChange}
+                    value={formik.values.firstName}
+                    label="First Name"
+                />
+                <CustomTextField
+                    id="lastName"
+                    name="lastName"
+                    onChange={formik.handleChange}
+                    value={formik.values.lastName}
+                    label="Last Name"
+                />
+                <CustomTextField
+                    id="age"
+                    name="age"
+                    onChange={formik.handleChange}
+                    type="number"
+                    value={formik.values.age}
+                    label="Age"
+                />
+                <CustomTextField
+                    id="email"
+                    name="email"
+                    onChange={formik.handleChange}
+                    value={formik.values.email}
+                    label="Email"
+                />
                 <Button onClick={() => formik.handleSubmit()} variant="contained">Contained</Button>
             </form>
         </div>
