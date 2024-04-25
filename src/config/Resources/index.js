94f2c0dc146c79f532a63141a3556b8c7249028c
Ishant Sahni <ishantsahni888@gmail.com>
2024-04-25 17:22:27 +0530

    images added

---
+++ src/config/Resources/index.js
@@ -0,0 +1,10 @@
+const base = "/images"
+
+const Resources = {
+    images: {
+        dropDownIcon: `${base}/DropdownIcon.svg`,
+        logo1: `${base}/logo192.png`
+    }
+}
+
+export default Resources;
