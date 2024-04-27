9b5e7437c626cfac5838ef4f40f57836c7560ecf
Ishant Sahni <ishantsahni888@gmail.com>
2024-04-28 03:08:41 +0530

    dfd

--- src/components/CustomRadioButton/index.jsx
+++ src/components/CustomRadioButton/index.jsx
@@ -1,7 +1,25 @@
+import { Radio, RadioGroup } from "@mui/material";
+
 function CustomRadioButton() {
-    return (
-        <div>Custom Radio Button</div>
-    )
+  const radioCount = 5;
+  return (
+    <div>
+      <RadioGroup
+        row
+        aria-labelledby="demo-form-control-label-placement"
+        name="position"
+        defaultValue="top"
+      >
+        {Array.from({ length: radioCount }, (_, index) => (
+          <Radio
+            key={index}
+            value={`option-${index + 1}`}
+            inputProps={{ "aria-label": `Option ${index + 1}` }}
+          />
+        ))}
+      </RadioGroup>
+    </div>
+  );
 }
 
 export default CustomRadioButton;
