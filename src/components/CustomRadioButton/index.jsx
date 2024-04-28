c2d14ba680133e243381b4a44c4c99daa2276e78
Ishant Sahni <ishantsahni888@gmail.com>
2024-04-28 17:45:28 +0530

    d

--- src/components/CustomRadioButton/index.jsx
+++ src/components/CustomRadioButton/index.jsx
@@ -1,22 +1,33 @@
 import { FormControlLabel, Radio, RadioGroup } from "@mui/material";
 import { useState } from "react";
 
-function CustomRadioButton({name, value, onChange}) {
+function CustomRadioButton({ name, value, onChange }) {
   const radioCount = value.length;
   return (
     <div>
-      <RadioGroup
-        row
-        aria-labelledby="demo-form-control-label-placement"
-        name={name}
-        value={value}
-        onChange={onChange}
-        defaultValue="top"
-      >
-        {Array.from({ length: radioCount }, (_, index) => (
-          <FormControlLabel key={index} labelPlacement="top" value={`${index+1}`} control={<Radio />} label={`${index+1}`} />
-        ))}
-      </RadioGroup>
+      {Array.from({ length: value.length }, (_, index) => {
+        return (<div key={index} className="flex">
+          <p>Option {index + 1}</p>
+          <RadioGroup
+            row
+            aria-labelledby="demo-form-control-label-placement"
+            name={name}
+            value={value[index]}
+            onChange={onChange}
+            defaultValue="top"
+          >
+            {Array.from({ length: value.length }, (_, radioIndex) => (
+              <FormControlLabel
+                key={radioIndex}
+                labelPlacement="top"
+                value={`${radioIndex + 1}`}
+                control={<Radio />}
+                label={`${radioIndex + 1}`}
+              />
+            ))}
+          </RadioGroup>
+        </div>);
+      })}
     </div>
   );
 }
