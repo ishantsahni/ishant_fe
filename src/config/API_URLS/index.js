356af15f2eecb4a2394df06021cf45338abb37af
Ishant Sahni <ishantsahni888@gmail.com>
2024-07-08 17:11:25 +0530

    path added

--- src/config/API_URLS/index.js
+++ src/config/API_URLS/index.js
@@ -2,8 +2,8 @@ const API_URLS = {
   // baseUrl: process.env.REACT_APP_BASE_URL,
   baseUrl: "http://localhost:4000",
   firstApi: "/firstApi",
-  postUserData: "/postUserData",
-  getUserData: "/getUserData"
+  postUserData: "/user/postUserData",
+  getUserData: "/user/getUserData"
 };
 
 export default API_URLS;
