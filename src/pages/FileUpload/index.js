0d5d8ea7f5510c4df75ee2d6fd6d387cf6b6788f
Ishant Sahni <ishantsahni888@gmail.com>
2024-07-10 14:22:08 +0530

    file upload component added

--- src/pages/FileUpload/index.js
+++ src/pages/FileUpload/index.js
@@ -1,6 +1,36 @@
+import axios from "axios";
+import { useState } from "react";
+
 function FileUpload() {
+    const [file, setFile] = useState(null);
+
+    const handleFileChange = (e) => {
+        setFile(e.target.files[0]);
+    };
+
+    const handleFileUpload = async () => {
+        const formData = new FormData();
+        formData.append('file', file);
+
+        try {
+            const res = await axios.post("http://localhost:4000/upload", formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            })
+            console.log("file upload response", res?.data);
+        } catch (error) {
+            console.error("file upload error", error);
+        }
+
+    }
+
+
     return (
-        <div>File Upload</div>
+        <div>
+            <input type="file" onChange={handleFileChange} />
+            <button onClick={handleFileUpload}>Upload</button>
+        </div>
     )
 };
 
