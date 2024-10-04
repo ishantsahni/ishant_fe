674f8215b15ae61e0075b6a37c791e61c1bfff73
Ishant Sahni <ishantsahni888@gmail.com>
2024-10-04 16:36:03 +0530

    id extracted

--- src/pages/ShowSingleProductPage/index.jsx
+++ src/pages/ShowSingleProductPage/index.jsx
@@ -1,6 +1,9 @@
+import { useParams } from "react-router-dom";
 import SingleProductComponent from "../../components/SingleProductComponent";
 
 function ShowSingleProductPage() {
+  const { id } = useParams();
+
   return (
     <div className="p-10">
       <p>Redirected to single product page</p>
