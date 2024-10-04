9c88478fc682e424d67c4b393166a9bb304da181
Ishant Sahni <ishantsahni888@gmail.com>
2024-10-04 14:11:53 +0530

    grid added

--- src/pages/ShowProductsPage/index.jsx
+++ src/pages/ShowProductsPage/index.jsx
@@ -1,21 +1,27 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import API_URLS from "../../config/API_URLS";
+import SingleProductComponent from "../../components/SingleProductComponent";
 
 function ShowProductsPage() {
-    const [allProducts, setAllProducts] = useState([]);
+  const [allProducts, setAllProducts] = useState([]);
 
-    useEffect(() => {
-        axios.get(`${API_URLS.baseUrl}${API_URLS.getProducts}`)
-        .then(response => setAllProducts(response?.data))
-        .catch(error => console.log("Error while fetch all products ", error))
-    }, []);
+  useEffect(() => {
+    axios
+      .get(`${API_URLS.baseUrl}${API_URLS.getProducts}`)
+      .then((response) => setAllProducts(response?.data))
+      .catch((error) => console.log("Error while fetch all products ", error));
+  }, []);
 
-    console.log("all products ", allProducts);
+  console.log("all products ", allProducts);
 
-    return (
-        <div>Show Products Page</div>
-    )
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+      {allProducts.map(() => (
+        <SingleProductComponent />
+      ))}
+    </div>
+  );
 }
 
 export default ShowProductsPage;
