aa12427309c76bd62d4e3756a9adb3cb9e1baa65
Ishant Sahni <ishantsahni888@gmail.com>
2024-10-04 14:08:40 +0530

    formatter added

--- src/components/SingleProductComponent/index.jsx
+++ src/components/SingleProductComponent/index.jsx
@@ -1,18 +1,19 @@
 function SingleProductComponent() {
-    return (
-        <div className="h-fit w-[10rem] bg-orange-200 flex flex-col items-center justify-center py-4 rounded-lg">
-            
-                <img className="w-[5rem] h-[5rem]" alt="eCommProductImage" src="https://ishantecommerce.s3.eu-north-1.amazonaws.com/file-1727936688715.jfif" />
-            
-            <p>Product Name</p>
-            <p>Description</p>
-            <p>Price</p>
-            <p>Category</p>
-            <p>Brand</p>
-            <p>Stock</p>
-    
-        </div>
-    )
+  return (
+    <div className="h-fit w-[10rem] bg-orange-200 flex flex-col items-center justify-center py-4 rounded-lg">
+      <img
+        className="w-[5rem] h-[5rem]"
+        alt="eCommProductImage"
+        src="https://ishantecommerce.s3.eu-north-1.amazonaws.com/file-1727936688715.jfif"
+      />
+      <p>Product Name</p>
+      <p>Description</p>
+      <p>Price</p>
+      <p>Category</p>
+      <p>Brand</p>
+      <p>Stock</p>
+    </div>
+  );
 }
 
 export default SingleProductComponent;
