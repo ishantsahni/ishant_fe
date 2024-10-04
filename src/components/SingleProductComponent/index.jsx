18b78e1ef6287b5f8c4473b8fe4e36f6a7fdb527
Ishant Sahni <ishantsahni888@gmail.com>
2024-10-04 16:34:55 +0530

    redirection added

--- src/components/SingleProductComponent/index.jsx
+++ src/components/SingleProductComponent/index.jsx
@@ -1,6 +1,14 @@
+import { useNavigate } from "react-router-dom";
+
 function SingleProductComponent({ productInfo }) {
+  const navigate = useNavigate();
   return (
-    <div className="h-fit w-[10rem] bg-orange-200 flex flex-col items-center justify-center py-4 rounded-lg">
+    <div
+      onClick={() => navigate(`/showSingleProduct/${productInfo?._id}`)}
+      onKeyDown={() => navigate(`/showSingleProduct/${productInfo?._id}`)}
+      role="presentation"
+      className="h-fit w-[10rem] cursor-pointer bg-orange-200 flex flex-col items-center justify-center py-4 rounded-lg"
+    >
       <img
         className="w-[5rem] h-[5rem]"
         alt="eCommProductImage"
--- src/pages/ShowSingleProductPage/index.jsx
+++ src/pages/ShowSingleProductPage/index.jsx
@@ -1,9 +1,11 @@
 import SingleProductComponent from "../../components/SingleProductComponent";
 
 function ShowSingleProductPage() {
-    return (
-        <div className="p-10"><SingleProductComponent /></div>
-    )
+  return (
+    <div className="p-10">
+      <p>Redirected to single product page</p>
+    </div>
+  );
 }
 
 export default ShowSingleProductPage;
