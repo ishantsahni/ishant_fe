3e5e799c71ea87e6a201a3c7cca193b50ab8503f
Ishant Sahni <ishantsahni888@gmail.com>
2024-10-12 11:49:06 +0530

    changed api urls

--- src/config/API_URLS/index.js
+++ src/config/API_URLS/index.js
@@ -1,9 +1,9 @@
 const API_URLS = {
   // baseUrl: process.env.REACT_APP_BASE_URL,
   baseUrl: "http://localhost:4000",
-  addProduct: "/addProduct",
-  getProducts: "/getProducts",
-  getProduct: "/getProduct"
+  addProduct: "/product/add",
+  getProducts: "/product/get",
+  getProduct: "/product/get"
 };
 
 export default API_URLS;
