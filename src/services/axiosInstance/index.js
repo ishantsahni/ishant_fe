9d7131ec094cf29ec886366d1916a6b0f9dea666
Ishant Sahni <ishantsahni888@gmail.com>
2024-10-28 22:54:39 +0530

    bearer token being sent

--- src/services/axiosInstance/index.js
+++ src/services/axiosInstance/index.js
@@ -9,7 +9,7 @@ const axiosInstance = axios.create({
 // Add a request interceptor
 axiosInstance.interceptors.request.use(
   (config) => {
-    const token = localStorage.getItem("accessToken");
+    const token = sessionStorage.getItem("accessToken");
 
     if (token) {
       config.headers["Authorization"] = `Bearer ${token}`;
