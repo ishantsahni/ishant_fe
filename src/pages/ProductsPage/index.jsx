c63302ebf36845328c5261f39f79091044fcd4a3
Ishant Sahni <ishantsahni888@gmail.com>
2024-10-30 12:41:46 +0530

    text field and dropdown added

--- src/pages/ProductsPage/index.jsx
+++ src/pages/ProductsPage/index.jsx
@@ -4,6 +4,8 @@ import * as yup from "yup";
 import SingleProductComponent from "../../components/SingleProductComponent";
 import axiosInstance from "../../services/axiosInstance";
 import { useFormik } from "formik";
+import CustomTextField from "../../components/CustomTextField";
+import CustomDropdown from "../../components/CustomDropdown";
 
 function ProductsPage() {
   const [allProducts, setAllProducts] = useState([]);
@@ -40,6 +42,39 @@ function ProductsPage() {
 
   return (
     <div>
+      <div className="flex">
+        <CustomDropdown
+          id="category"
+          name="category"
+          onChange={formik.handleChange}
+          onBlur={formik.handleChange}
+          value={formik.values.category}
+          label="Category"
+          touched={formik.touched.category}
+          errors={formik.touched.errors}
+        />
+        <CustomTextField
+          id="price"
+          name="price"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          type="number"
+          value={formik.values.price}
+          label="Price"
+          touched={formik.touched.price}
+          errors={formik.errors.price}
+        />
+        <CustomTextField
+          id="search"
+          name="search"
+          onChange={formik.handleChange}
+          onBlur={formik.handleBlur}
+          value={formik.values.search}
+          label="Brand"
+          touched={formik.touched.search}
+          errors={formik.errors.search}
+        />
+      </div>
       <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
         {allProducts.map((item) => (
           <SingleProductComponent key={item?._id} productInfo={item} />
