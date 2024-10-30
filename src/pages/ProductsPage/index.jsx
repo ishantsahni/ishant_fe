4ee8d21044e2d60b688902d6cb5e3de6a8a2d94a
Ishant Sahni <ishantsahni888@gmail.com>
2024-10-30 12:39:52 +0530

    formik added

--- src/pages/ProductsPage/index.jsx
+++ src/pages/ProductsPage/index.jsx
@@ -1,10 +1,35 @@
 import { useEffect, useState } from "react";
 import API_URLS from "../../config/API_URLS";
+import * as yup from "yup";
 import SingleProductComponent from "../../components/SingleProductComponent";
 import axiosInstance from "../../services/axiosInstance";
+import { useFormik } from "formik";
 
 function ProductsPage() {
   const [allProducts, setAllProducts] = useState([]);
+
+  const formik = useFormik({
+    initialValues: {
+      category: "",
+      price: 0,
+      search: "",
+    },
+    validationSchema: yup.object({
+      category: yup.string(),
+      price: yup.number("Must be a number"),
+      search: yup.string(),
+    }),
+
+    onSubmit: (values) => {
+      console.log("formik submitted ", values);
+      // axiosInstance
+      //   .post(`${API_URLS.baseURL}${API_URLS.addProduct}`, values)
+      //   .then((response) =>
+      //     console.log("Product added successfully: ", response.data)
+      //   )
+      //   .catch((error) => console.log("Error adding products: ", error));
+    },
+  });
 
   useEffect(() => {
     axiosInstance
@@ -13,13 +38,13 @@ function ProductsPage() {
       .catch((error) => console.log("Error while fetch all products ", error));
   }, []);
 
-  console.log("all products ", allProducts);
-
   return (
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-      {allProducts.map((item) => (
-        <SingleProductComponent key={item?._id} productInfo={item} />
-      ))}
+    <div>
+      <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
+        {allProducts.map((item) => (
+          <SingleProductComponent key={item?._id} productInfo={item} />
+        ))}
+      </div>
     </div>
   );
 }
