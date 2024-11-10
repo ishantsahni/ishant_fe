270c3839d3465ddd94118864dcf06d357937e5f0
Ishant Sahni <ishantsahni888@gmail.com>
2024-11-10 14:25:09 +0530

    user _id updated to userId

--- src/components/ProtectedRoute/index.js
+++ src/components/ProtectedRoute/index.js
@@ -1,32 +1,32 @@
-import React from 'react';
-import { Navigate } from 'react-router-dom';
-import { jwtDecode } from 'jwt-decode';
-
+import React from "react";
+import { Navigate } from "react-router-dom";
+import { jwtDecode } from "jwt-decode";
 
 const ProtectedRoute = ({ element: Component }) => {
+  const accessToken = sessionStorage.getItem("accessToken");
 
-    const accessToken = sessionStorage.getItem('accessToken');
+  if (!accessToken) {
+    return <Navigate to="/" />;
+  }
 
-    if (!accessToken) {
-        return <Navigate to="/" />
+  try {
+    const decodedToken = jwtDecode(accessToken);
+    const time = Date.now() / 1000;
+    if (decodedToken.exp < time) {
+      sessionStorage.removeItem("accessToken");
+      return <Navigate to="/" />;
     }
+  } catch (error) {
+    console.log("Invalid token or decoding error ", error);
+    sessionStorage.removeItem("accessToken");
+    return <Navigate to="/" />;
+  }
 
-    try {
-        const decodedToken = jwtDecode(accessToken);
-        const time = Date.now() / 1000;
-        if (decodedToken.exp < time) {
-            sessionStorage.removeItem('accessToken');
-            return <Navigate to="/" />
-        }
-    } catch (error) {
-        console.log("Invalid token or decoding error ", error);
-        sessionStorage.removeItem('accesstToken');
-        return <Navigate to="/" />
-    }
-
-    return (
-        <div><Component /></div>
-    )
-}
+  return (
+    <div>
+      <Component />
+    </div>
+  );
+};
 
 export default ProtectedRoute;
