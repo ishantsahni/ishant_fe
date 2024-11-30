a564fa55c46eaed73b60aa41eef2f057f7a577c3
Ishant Sahni <ishantsahni888@gmail.com>
2024-11-30 11:15:11 +0530

    design added

--- src/components/ReviewComponent/index.js
+++ src/components/ReviewComponent/index.js
@@ -1,5 +1,16 @@
+import { FaStar } from "react-icons/fa";
+
 function ReviewComponent() {
-  return <div>Review Component</div>;
+  return (
+    <div className="p-2 bg-lime-200 rounded-xl">
+      <p>Ishant Sahni</p>
+      <div className="flex my-2">
+        <FaStar />
+        <FaStar />
+      </div>
+      <p>I used it, the product is really good!!</p>
+    </div>
+  );
 }
 
 export default ReviewComponent;
