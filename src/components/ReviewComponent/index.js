ba902c124053e4558101124f0eaefedc7a0ff60c
Ishant Sahni <ishantsahni888@gmail.com>
2024-12-05 00:43:40 +0530

    reviews added

--- src/components/ReviewComponent/index.js
+++ src/components/ReviewComponent/index.js
@@ -3,12 +3,12 @@ import { FaStar } from "react-icons/fa";
 function ReviewComponent({ reviewInfo }) {
   console.log("review info , ", reviewInfo);
   return (
-    <div className="p-2 bg-lime-200 rounded-xl">
+    <div className="p-2 mt-4 bg-lime-200 rounded-xl">
       <p>{reviewInfo?.user?.name}</p>
       <div className="flex my-2">
-        {/* {reviewInfo?.rating.map(() => ( */}
-        <FaStar />
-        {/* ))} */}
+        {[...Array(reviewInfo?.rating)].map((_, index) => (
+          <FaStar key={index} />
+        ))}
       </div>
       <p>{reviewInfo?.comment}</p>
     </div>
